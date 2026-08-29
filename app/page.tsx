eb4e8851977751298eafd14ffdb40d3fbed1d671
capacitytools <capacitydotbuild@gmail.com>
2026-08-29 14:02:22 +0100

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ import { GChatApp } from "@/components/gchat-app";
 
 export default function HomePage() {
   return (
-    <main>
+    <main className="min-h-screen bg-gbackground dark:bg-gdark-background">
       <GChatApp />
     </main>
   );
